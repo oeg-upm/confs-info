6333417629eeb9c31348cdc6696b269da725eaff
Ahmad Alobaid <ahmad88me@gmail.com>
2019-04-18 19:04:05 +0200

    update journals

---
+++ journals.js
@@ -0,0 +1,48 @@
+var journals_data = [
+    {
+   "Q":"Q1",
+   "name":"Knowledge-based Systems",
+   "topics":"",
+   "impact_factor":4.396,
+   "comments":"<a href='https://www.journals.elsevier.com/knowledge-based-systems'>website</a>"
+},
+  
+        {
+   "Q":"Q1",
+   "name":"Communications of the ACM",
+   "topics":"",
+   "impact_factor":3.063,
+   "comments":""
+},
+    
+        {
+   "Q":"Q1",
+   "name":"Artificial Intelligence",
+   "topics":"General AI",
+   "impact_factor":3.034,
+   "comments":""
+},
+    
+{
+   "Q":"Q1/Q2",
+   "name":"IEEE Intelligent Systems",
+   "topics":"",
+   "impact_factor":2.596,
+   "comments":""
+},
+{
+   "Q":"Q1",
+   "name":"Scientific Data (Nature)",
+   "topics":"",
+   "impact_factor":5.305,
+   "comments":"<a href='https://www.nature.com/sdata/'>website</a>"
+},
+  {
+   "Q":"Q1",
+   "name":"Information Sciences",
+   "topics":"",
+   "impact_factor":4.305,
+   "comments":"<a href='https://www.journals.elsevier.com/information-sciences'>website</a>"
+},  
+    
+]
