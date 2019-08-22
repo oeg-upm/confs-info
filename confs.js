5cb9257218a07e35ca20be1b12cb1a37c0d31cb6
Ahmad Alobaid <ahmad88me@gmail.com>
2019-08-22 15:21:23 +0200

    update list

--- confs.js
+++ confs.js
@@ -1,64 +1,57 @@
 var conf_data = [
-// {
-//    "core/Q":"A",
-//    "title":"35th IEEE International Conference on Data Engineering (ICDE 2019)",
-//    "topics":`Benchmarking, Performance Modelling, and Tuning
-// Data Integration, Metadata Management, and Interoperability
-// Data Mining and Knowledge Discovery
-// Data Models, Semantics, Query languages
-// Data Provenance, cleaning, curation
-// Data Science
-// Data Stream Systems and Sensor Networks
-// Data Visualization and Interactive Data Exploration
-// Database Privacy, Security, and Trust
-// Distributed, Parallel and P2P Data Management
-// Graphs, RDF, Web Data and Social Networks
-// Database technology for machine learning
-// Modern Hardware and In-Memory Database Systems
-// Query Processing, Indexing, and Optimization
-// Search and Information extraction
-// Strings, Texts, and Keyword Search
-// Temporal, Spatial, Mobile and Multimedia
-// Uncertain, Probabilistic Databases
-// Workflows, Scientific Data Management`,
-//    "type":"Conference",
-//    "submission_date":"October 8, 2018",
-//    "notification_date": "December 14, 2018",
-//    "event_date":"April 8-12, 2019",
-//    "comments":'<a href="http://conferences.cis.umac.mo/icde2019/?page_id=43"> website</a><br> This conference has two rounds of submissions.'
-// },
-// {
-//    "core/Q":"A",
-//    "title":"Extending Database Technology (EDBT)",
-//    "topics":` Availability, reliability, and scalability
-//         Big data storage, processing and transformation
-//         Complex event processing and data streams
-//         Concurrency control, recovery, and transaction management
-//         Data management in clouds
-//         <strong>Data mining and knowledge discovery</strong>
-//         <strong>Data quality, curation, and provenance</strong>
-//         Data warehousing, large-scale analytics and ETL tools
-//         Emerging hardware and in-memory databases
-//         Experiments and analysis
-//         <strong>Graph databases and semantic web</strong>
-//         Heterogeneous databases, data integration
-//         Middleware and workflow management
-//         Parallel, distributed and grid data management
-//         Privacy, trust and security in databases
-//         Query processing and optimization
-//         Scientific and statistical databases
-//         <strong>Semi-structured and linked data management</strong>
-//         <strong>Sensor and mobile data management</strong>
-//         Social networks and crowdsourcing
-//         Spatial, temporal, and geographic databases
-//         <strong>Text databases and information retrieval</strong>
-//         Tuning, monitoring, benchmarking and performance evaluation
-//         User interfaces and data visualization`,
-//    "type":"Conference",
-//    "submission_date":"October 8, 2018",
-//    "event_date":"March 26-29, 2019",
-//    "comments":'<a href="http://edbticdt2019.inesc-id.pt/?EDBT_CFP">website</a>'
-// },
+ {
+    "core/Q":"A",
+    "title":"Extending Database Technology (EDBT)",
+    "topics":` Availability, reliability, and scalability
+Big data storage, processing and transformation
+Complex event processing and data streams
+Concurrency control, recovery, and transaction management
+Data management in clouds
+Data mining and knowledge discovery
+Data quality, curation, and provenance
+Data warehousing, large-scale analytics and ETL tools
+Emerging hardware and in-memory databases
+Experiments and analysis
+Graph databases and semantic web
+Heterogeneous databases, data integration
+Middleware and workflow management
+Parallel, distributed and grid data management
+Privacy, trust and security in databases
+Query processing and optimization
+Scientific and statistical databases
+Semi-structured and linked data management
+Sensor and mobile data management
+Social networks and crowdsourcing
+Spatial, temporal, and geographic databases
+Text databases and information retrieval
+Tuning, monitoring, benchmarking and performance evaluation
+User interfaces and data visualization
+Visionary`,
+    "type":"Conference",
+    "submission_date":"2019-10-7",
+    "notification_date": "2019-11-22",
+    "event_date":"",
+    "comments":'<a href="https://diku-dk.github.io/edbticdt2020/?contents=important_dates.html">website</a> Joint Conference EDBT/ICDT, the dates are for EDBT'
+ },
+  {
+    "core/Q":"A",
+    "title":"ICDT",
+    "topics":`Data mining, information extraction, information retrieval, and database aspects of machine learning
+Data models, design, structures, semantics, query languages, and algorithms for data management
+Distributed and parallel databases, cloud computing
+Connections between databases and knowledge representation
+Graph databases, (semantic) Web data, and Web services
+Data streams and sketching
+Data-centric (business) process management and workflows
+Data and knowledge integration and exchange, data provenance, views, and data warehouses
+Domain-specific databases (multimedia, scientific, spatial, temporal, text)
+Data privacy and security, concurrency, and recovery`,
+    "type":"Conference",
+    "submission_date":"2019-09-15",
+    "notification_date": "2019-12-05",
+    "event_date":"",
+    "comments":'<a href="https://diku-dk.github.io/edbticdt2020/?contents=ICDT_CFP.html">website</a> Joint Conference The data are for ICDT here'
+ },
  {
     "core/Q": "Q1",
     "title": "Future Generation Computer Systems (Special Issue)",
@@ -79,15 +72,15 @@ var conf_data = [
     "event_date": "2020-02-03",
     "comments": '<a href="http://www.wsdm-conference.org/2020/">website</a>'
  },
-// {
-//    "core/Q":"A",
-//    "title":"Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD)",
-//    "topics":"<strong>knowledge discovery</strong>, data mining, data warehousing, machine learning, artificial intelligence, databases, statistics, <strong>knowledge engineering</strong>, visualization, decision-making systems, ...",
-//    "type":"Conference",
-//    "submission_date":"October 10, 2018",
-//    "event_date":"April 14-17, 2019",
-//    "comments":'<a href="http://pakdd2019.medmeeting.org/Content/92892"> website</a>'
-// },
+ {
+    "core/Q":"A",
+    "title":"Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD)",
+    "topics":"<strong>knowledge discovery</strong>, data mining, data warehousing, machine learning, artificial intelligence, databases, statistics, <strong>knowledge engineering</strong>, visualization, decision-making systems, ...",
+    "type":"Conference",
+    "submission_date":"",
+    "event_date":"",
+    "comments":''
+ },
 {
     "core/Q":"A*",
     "title":"TheWebConf (WWW)",
@@ -113,31 +106,27 @@ var conf_data = [
    "title":"Extended Semantic Web Conference (ESWC)",
    "topics":"semantic web",
    "type":"Conference",
-//   "submission_date":"03-12-2018",
-//   "notification_date": "18-02-2019",
-   "submission_date":"2018-12-03",
-   "notification_date": "2019-02-18",
+   "submission_date":"",
+   "notification_date": "",
    "event_date":"",
-   "comments":'<a href="https://2019.eswc-conferences.org/">website</a>'
+   "comments":''
 },
 {
    "core/Q":"A",
    "title":"International Semantic Web Conference (ISWC)",
    "topics":"semantic web",
    "type":"Conference",
-//   "submission_date":"03-04-2019",
-   "submission_date":"2019-04-03",
-   "notification_date": "2019-05-22",
-   "event_date":"2019-10-26",
-   "comments":"<a href='https://iswc2019.semanticweb.org/'>website</a>"
+   "submission_date":"",
+   "notification_date": "",
+   "event_date":"=",
+   "comments":""
 },
 {
    "core/Q":"B",
    "title":"Internation Conference on Web Engineering (ICWE)",
    "topics":"semantic web",
    "type":"Conference",
-//   "submission_date":"18-01-2019",
-   "submission_date":"2019-01-18",
+   "submission_date":"",
    "notification_date": "",
    "event_date":"",
    "comments":""
@@ -147,9 +136,6 @@ var conf_data = [
    "title":"International Conference on Software Engineering and Knowledge Engineering (SEKE)",
    "topics":"knowledge engineering",
    "type":"Conference",
-//   "submission_date":"01-03-2019",
-//   "notification_date": "20-04-2019",
-//   "event_date":"10-07-2019",
    "submission_date":"2019-03-01",
    "notification_date": "2019-04-20",
    "event_date":"2019-07-10",
@@ -180,9 +166,9 @@ var conf_data = [
    "title":"The IEEE International Conference on Data Engineering (ICDE)",
    "topics":"Data, Information, Knowledge, Semantics, ...",
    "type":"Conference",
-   "submission_date":"2019-06-08",
-   "notification_date": "2019-08-10",
+   "submission_date":"2019-10-08",
+   "notification_date": "2019-12-14",
    "event_date":"2020-04-20",
-   "comments":"<a href='https://www.utdallas.edu/icde/call.html'>website</a>"
+   "comments":"second round <a href='https://www.utdallas.edu/icde/call.html'>website</a>"
 },
 ]
