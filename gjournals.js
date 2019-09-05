8ce0d41947b4491fe13ad83706e29c300d9aab61
Ahmad Alobaid <ahmad88me@gmail.com>
2019-09-05 18:27:14 +0200

    JCR ranking to all journals

--- gjournals.js
+++ gjournals.js
@@ -3,46 +3,46 @@ var journals_data = [
    "Q":"Q1",
    "name":"Knowledge-based Systems",
    "topics":"",
-   "impact_factor":4.396,
-   "comments":"<a href='https://www.journals.elsevier.com/knowledge-based-systems'>website</a>"
+   "impact_factor":5.101,
+   "comments":"<a href='https://www.journals.elsevier.com/knowledge-based-systems'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=KNOWL-BASED%20SYST&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },
   
         {
    "Q":"Q1",
    "name":"Communications of the ACM",
    "topics":"",
-   "impact_factor":3.063,
-   "comments":""
+   "impact_factor":5.410,
+   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=COMMUN%20ACM&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },
-    
-        {
+
+{
    "Q":"Q1",
    "name":"Artificial Intelligence",
    "topics":"General AI",
-   "impact_factor":3.034,
-   "comments":""
+   "impact_factor":4.483,
+   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=ARTIF%20INTELL&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },
     
 {
-   "Q":"Q1/Q2",
+   "Q":"Q1",
    "name":"IEEE Intelligent Systems",
    "topics":"",
-   "impact_factor":2.596,
-   "comments":""
+   "impact_factor":4.464,
+   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=IEEE%20INTELL%20SYST&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },
 {
    "Q":"Q1",
    "name":"Scientific Data (Nature)",
    "topics":"",
-   "impact_factor":5.305,
-   "comments":"<a href='https://www.nature.com/sdata/'>website</a>"
+   "impact_factor":5.929,
+   "comments":"<a href='https://www.nature.com/sdata/'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=SCI%20DATA&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },
   {
    "Q":"Q1",
    "name":"Information Sciences",
    "topics":"",
-   "impact_factor":4.305,
-   "comments":"<a href='https://www.journals.elsevier.com/information-sciences'>website</a>"
+   "impact_factor":5.524,
+   "comments":"<a href='https://www.journals.elsevier.com/information-sciences'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=INFORM%20SCIENCES&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
 },  
     
 ]
