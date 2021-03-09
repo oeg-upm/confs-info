ea9db592931b9e185241ca71e655fc1261b2dd8c
Ahmad Alobaid <ahmad88me@gmail.com>
2021-03-09 22:09:08 +0100

    update the time of ISWC

--- confs.js
+++ confs.js
@@ -1,8 +1,8 @@
 var conf_data = [
- {
-    "core/Q":"A",
-    "title":"Extending Database Technology (EDBT)",
-    "topics":` Availability, reliability, and scalability
+    {
+        "core/Q": "A",
+        "title": "Extending Database Technology (EDBT)",
+        "topics": ` Availability, reliability, and scalability
 Big data storage, processing and transformation
 Complex event processing and data streams
 Concurrency control, recovery, and transaction management
@@ -27,16 +27,16 @@ Text databases and information retrieval
 Tuning, monitoring, benchmarking and performance evaluation
 User interfaces and data visualization
 Visionary`,
-    "type":"Conference",
-    "submission_date":"2019-10-07",
-    "notification_date": "2019-11-22",
-    "event_date":"2020-03-30",
-    "comments":'<a href="https://diku-dk.github.io/edbticdt2020/?contents=important_dates.html">website</a> Joint Conference EDBT/ICDT, the dates are for EDBT'
+        "type": "Conference",
+        "submission_date": "2019-10-07",
+        "notification_date": "2019-11-22",
+        "event_date": "2020-03-30",
+        "comments": '<a href="https://diku-dk.github.io/edbticdt2020/?contents=important_dates.html">website</a> Joint Conference EDBT/ICDT, the dates are for EDBT'
  },
-  {
-    "core/Q":"A",
-    "title":"ICDT",
-    "topics":`Data mining, information extraction, information retrieval, and database aspects of machine learning
+    {
+        "core/Q": "A",
+        "title": "ICDT",
+        "topics": `Data mining, information extraction, information retrieval, and database aspects of machine learning
 Data models, design, structures, semantics, query languages, and algorithms for data management
 Distributed and parallel databases, cloud computing
 Connections between databases and knowledge representation
@@ -46,130 +46,130 @@ Data-centric (business) process management and workflows
 Data and knowledge integration and exchange, data provenance, views, and data warehouses
 Domain-specific databases (multimedia, scientific, spatial, temporal, text)
 Data privacy and security, concurrency, and recovery`,
-    "type":"Conference",
-    "submission_date":"2019-09-15",
-    "notification_date": "2019-12-05",
-    "event_date":"2020-03-30",
-    "comments":'<a href="https://diku-dk.github.io/edbticdt2020/?contents=ICDT_CFP.html">website</a> Joint Conference The data are for ICDT here'
+        "type": "Conference",
+        "submission_date": "2019-09-15",
+        "notification_date": "2019-12-05",
+        "event_date": "2020-03-30",
+        "comments": '<a href="https://diku-dk.github.io/edbticdt2020/?contents=ICDT_CFP.html">website</a> Joint Conference The data are for ICDT here'
  },
- {
-    "core/Q": "Q1",
-    "title": "Future Generation Computer Systems (Special Issue)",
-    "topics": "Special Issue on Machine Learning and Knowledge Graphs",
-    "type": "Journal",
-    "submission_date": "2019-11-30",
-    "notification_date": "2020-01-31",
-    "event_date": "2020-07-31",
-    "comments": '<a href="https://www.journals.elsevier.com/future-generation-computer-systems/call-for-papers/special-issue-on-machine-learning-and-knowledge-graphs">website</a>'
+    {
+        "core/Q": "Q1",
+        "title": "Future Generation Computer Systems (Special Issue)",
+        "topics": "Special Issue on Machine Learning and Knowledge Graphs",
+        "type": "Journal",
+        "submission_date": "2019-11-30",
+        "notification_date": "2020-01-31",
+        "event_date": "2020-07-31",
+        "comments": '<a href="https://www.journals.elsevier.com/future-generation-computer-systems/call-for-papers/special-issue-on-machine-learning-and-knowledge-graphs">website</a>'
  },
- {
-    "core/Q": "A+",
-    "title": "The 12th ACM International WSDM Conference on Web Search and Data Mining",
-    "topics": "Web, Data, NLP, ... ",
-    "type": "Conference",
-    "submission_date": "2019-08-12",
-    "notification_date": "2019-10-12",
-    "event_date": "2020-02-03",
-    "comments": '<a href="http://www.wsdm-conference.org/2020/">website</a>'
+    {
+        "core/Q": "A+",
+        "title": "The 12th ACM International WSDM Conference on Web Search and Data Mining",
+        "topics": "Web, Data, NLP, ... ",
+        "type": "Conference",
+        "submission_date": "2019-08-12",
+        "notification_date": "2019-10-12",
+        "event_date": "2020-02-03",
+        "comments": '<a href="http://www.wsdm-conference.org/2020/">website</a>'
  },
- {
-    "core/Q":"A",
-    "title":"Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD)",
-    "topics":"<strong>knowledge discovery</strong>, data mining, data warehousing, machine learning, artificial intelligence, databases, statistics, <strong>knowledge engineering</strong>, visualization, decision-making systems, ...",
-    "type":"Conference",
-    "notification_date": "2020-01-28",
-    "submission_date":"2019-11-18",
-    "event_date":"2020-05-11",
-    "comments":'<a href="https://www.pakdd2020.org">website</a>'
+    {
+        "core/Q": "A",
+        "title": "Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD)",
+        "topics": "<strong>knowledge discovery</strong>, data mining, data warehousing, machine learning, artificial intelligence, databases, statistics, <strong>knowledge engineering</strong>, visualization, decision-making systems, ...",
+        "type": "Conference",
+        "notification_date": "2020-01-28",
+        "submission_date": "2019-11-18",
+        "event_date": "2020-05-11",
+        "comments": '<a href="https://www.pakdd2020.org">website</a>'
  },
-{
-    "core/Q":"A+",
-    "title":"TheWebConf (WWW)",
-    "topics":"Semantic Web, Ontology Engineering, Web in general",
-    "type":"Conference",
-    "submission_date":"2019-10-07",
-    "notification_date": "2020-01-10",
-    "event_date":"2020-04-20",
-    "comments":'<a href="https://www2020.thewebconf.org/">website</a>'
+    {
+        "core/Q": "A+",
+        "title": "TheWebConf (WWW)",
+        "topics": "Semantic Web, Ontology Engineering, Web in general",
+        "type": "Conference",
+        "submission_date": "2019-10-07",
+        "notification_date": "2020-01-10",
+        "event_date": "2020-04-20",
+        "comments": '<a href="https://www2020.thewebconf.org/">website</a>'
  },
- {
-    "core/Q":"A",
-    "title":"Innovative Data Systems Research (CIDR)",
-    "topics":"cloud computing, data analysis, sensor databases, information integration, data architectures, information retrieval",
-    "type":"Conference",
-    "submission_date":"2019-08-25",
-    "notification_date": "2019-10-14",
-    "event_date":"2020-01-12",
-    "comments":'<a href="http://cidrdb.org/cidr2020/"> website</a>'
+    {
+        "core/Q": "A",
+        "title": "Innovative Data Systems Research (CIDR)",
+        "topics": "cloud computing, data analysis, sensor databases, information integration, data architectures, information retrieval",
+        "type": "Conference",
+        "submission_date": "2019-08-25",
+        "notification_date": "2019-10-14",
+        "event_date": "2020-01-12",
+        "comments": '<a href="http://cidrdb.org/cidr2020/"> website</a>'
  },
-{
-   "core/Q":"A",
-   "title":"Extended Semantic Web Conference (ESWC)",
-   "topics":"semantic web",
-   "type":"Conference",
-   "submission_date":"2019-12-04",
-   "notification_date": "2020-02-19",
-   "event_date":"2020-05-31",
-    "comments":'<a href="https://2020.eswc-conferences.org"> website</a>'
+    {
+        "core/Q": "A",
+        "title": "Extended Semantic Web Conference (ESWC)",
+        "topics": "semantic web",
+        "type": "Conference",
+        "submission_date": "2019-12-04",
+        "notification_date": "2020-02-19",
+        "event_date": "2020-05-31",
+        "comments": '<a href="https://2020.eswc-conferences.org"> website</a>'
 },
-{
-   "core/Q":"A",
-   "title":"International Semantic Web Conference (ISWC)",
-   "topics":"semantic web",
-   "type":"Conference",
-   "submission_date":"2020-03-27",
-   "notification_date": "2020-06-19",
-   "event_date":"2020-11-02",
-   "comments":'<a href="https://iswc2020.semanticweb.org"> website</a>'
+    {
+        "core/Q": "A",
+        "title": "International Semantic Web Conference (ISWC)",
+        "topics": "semantic web",
+        "type": "Conference",
+        "submission_date": "2021-04-12",
+        "notification_date": "2021-06-23",
+        "event_date": "2021-10-26",
+        "comments": '<a href="https://iswc2021.semanticweb.org/"> website</a>'
 },
-{
-   "core/Q":"B",
-   "title":"Internation Conference on Web Engineering (ICWE)",
-   "topics":"semantic web",
-   "type":"Conference",
-   "submission_date":"",
-   "notification_date": "",
-   "event_date":"",
-   "comments":""
+    {
+        "core/Q": "B",
+        "title": "Internation Conference on Web Engineering (ICWE)",
+        "topics": "semantic web",
+        "type": "Conference",
+        "submission_date": "",
+        "notification_date": "",
+        "event_date": "",
+        "comments": ""
 },
-{
-   "core/Q":"B",
-   "title":"International Conference on Software Engineering and Knowledge Engineering (SEKE)",
-   "topics":"knowledge engineering",
-   "type":"Conference",
-   "submission_date":"2019-03-01",
-   "notification_date": "2019-04-20",
-   "event_date":"2019-07-10",
-   "comments":"<a href='http://ksiresearchorg.ipage.com/seke/seke19.html'>website</a>"
+    {
+        "core/Q": "B",
+        "title": "International Conference on Software Engineering and Knowledge Engineering (SEKE)",
+        "topics": "knowledge engineering",
+        "type": "Conference",
+        "submission_date": "2019-03-01",
+        "notification_date": "2019-04-20",
+        "event_date": "2019-07-10",
+        "comments": "<a href='http://ksiresearchorg.ipage.com/seke/seke19.html'>website</a>"
 },
-{
-   "core/Q":"A+",
-   "title":"ACM International Conference on Knowledge Discovery and Data Mining (SIGKDD)",
-   "topics":"knowledge discovery",
-   "type":"Conference",
-   "submission_date":"2020-02-13",
-   "notification_date": "2020-05-15",
-   "event_date":"2020-08-22",
-   "comments":"<a href='https://www.kdd.org/kdd2020/'>website</a>"
+    {
+        "core/Q": "A+",
+        "title": "ACM International Conference on Knowledge Discovery and Data Mining (SIGKDD)",
+        "topics": "knowledge discovery",
+        "type": "Conference",
+        "submission_date": "2020-02-13",
+        "notification_date": "2020-05-15",
+        "event_date": "2020-08-22",
+        "comments": "<a href='https://www.kdd.org/kdd2020/'>website</a>"
 },
-{
-   "core/Q":"A",
-   "title":"International Conference on Knowledge Capture (K-CAP)",
-   "topics":"",
-   "type":"Conference",
-   "submission_date":"2019-07-20",
-   "notification_date": "2019-08-30",
-   "event_date":"2019-11-19",
-   "comments":"<a href='http://www.k-cap.org/2019/index.html'>website</a>"
+    {
+        "core/Q": "A",
+        "title": "International Conference on Knowledge Capture (K-CAP)",
+        "topics": "",
+        "type": "Conference",
+        "submission_date": "2019-07-20",
+        "notification_date": "2019-08-30",
+        "event_date": "2019-11-19",
+        "comments": "<a href='http://www.k-cap.org/2019/index.html'>website</a>"
 },
-{
-   "core/Q":"A",
-   "title":"The IEEE International Conference on Data Engineering (ICDE)",
-   "topics":"Data, Information, Knowledge, Semantics, ...",
-   "type":"Conference",
-   "submission_date":"2019-10-08",
-   "notification_date": "2019-12-14",
-   "event_date":"2020-04-20",
-   "comments":"second round <a href='https://www.utdallas.edu/icde/call.html'>website</a>"
+    {
+        "core/Q": "A",
+        "title": "The IEEE International Conference on Data Engineering (ICDE)",
+        "topics": "Data, Information, Knowledge, Semantics, ...",
+        "type": "Conference",
+        "submission_date": "2019-10-08",
+        "notification_date": "2019-12-14",
+        "event_date": "2020-04-20",
+        "comments": "second round <a href='https://www.utdallas.edu/icde/call.html'>website</a>"
 },
 ]
