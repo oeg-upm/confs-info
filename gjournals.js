08c5fc2c4704ef3b3cd573ad559ade2a3289649a
Ahmad Alobaid <aalobaid@Ahmads-MacBook-Pro.local>
2021-07-08 15:10:58 +0200

    update with jcr 2020

--- gjournals.js
+++ gjournals.js
@@ -1,48 +1,99 @@
 var journals_data = [
     {
-   "Q":"Q1",
+   "Q":"Q1 - Artificial Intelligence",
    "name":"Knowledge-based Systems",
    "topics":"",
-   "impact_factor":5.101,
-   "comments":"<a href='https://www.journals.elsevier.com/knowledge-based-systems'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=KNOWL-BASED%20SYST&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":8.038,
+   "comments":"JCR"
 },
   
         {
-   "Q":"Q1",
+   "Q":"Q1 - THEORY & METHODS<br>Q1 - SOFTWARE ENGINEERING<br>Q1 - HARDWARE & ARCHITECTURE",
    "name":"Communications of the ACM",
    "topics":"",
-   "impact_factor":5.410,
-   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=COMMUN%20ACM&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":4.654,
+   "comments":"JCR"
 },
 
 {
-   "Q":"Q1",
+   "Q":"Q1 - ARTIFICIAL INTELLIGENCE",
    "name":"Artificial Intelligence",
    "topics":"General AI",
-   "impact_factor":4.483,
-   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=ARTIF%20INTELL&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":9.088,
+   "comments":"JCR"
 },
     
 {
-   "Q":"Q1",
+   "Q":"Q2 - ELECTRICAL & ELECTRONIC<br>Q2 - ARTIFICIAL INTELLIGENCE",
    "name":"IEEE Intelligent Systems",
    "topics":"",
-   "impact_factor":4.464,
-   "comments":"<a href='https://apps.clarivate.com/jif/home/?journal=IEEE%20INTELL%20SYST&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":3.405,
+   "comments":"JCR"
 },
 {
-   "Q":"Q1",
+   "Q":"Q1 - MULTIDISCIPLINARY SCIENCES",
    "name":"Scientific Data (Nature)",
    "topics":"",
-   "impact_factor":5.929,
-   "comments":"<a href='https://www.nature.com/sdata/'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=SCI%20DATA&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":6.444,
+   "comments":"JCR"
 },
   {
-   "Q":"Q1",
+   "Q":"Q1 - INFORMATION SYSTEMS",
    "name":"Information Sciences",
    "topics":"",
-   "impact_factor":5.524,
-   "comments":"<a href='https://www.journals.elsevier.com/information-sciences'>website</a><br><a href='https://apps.clarivate.com/jif/home/?journal=INFORM%20SCIENCES&editions=SCIE&year=2018&pssid=J1-YoMn93IkGwOdnQ4k2aOFGEqNJe1oEbYjX-D8t3E1QZgo0VgM8xaFPS6jRCpilrKRyt3b3u7jPDnoAxka6FcEXL2MOAXtyvHJ5-qBgNuLRjcgZrPm66fhjx2Fmwx3Dx3D-h9tQNJ9Nv4eh45yLvkdX3gx3Dx3D'>JCR ranking</a>"
+   "impact_factor":6.795,
+   "comments":"JCR"
 },  
-    
+     {
+      "Q":"Q2 - Theory & Methods<br>Q3 - Information Systems<br>Q3 - Artificial Intelligence",
+      "name":"Semantic Web Journal",
+      "topics":"",
+      "impact_factor":2.214,
+      "comments":"JCR"
+   },
+                     
+     {
+      "Q":"Q3 - Information Systems<br>Q3 - Software Engineering<br>Q3 - Artificial Intelligence",
+      "name":"Journal of Web Semantics",
+      "topics":"",
+      "impact_factor":1.897,
+      "comments":"JCR"
+   },
+                     
+     {
+      "Q":"Q2 - Software Engineering<br>Q3 - Information Systems",
+      "name":"ACM Transactions on Knowledge Discovery from Data",
+      "topics":"",
+      "impact_factor":2.713,
+      "comments":"JCR"
+   },
+     {
+      "Q":"Q1 - Information Systems<br>Q1 - Artificial Intelligence<br>Q1 - Electrical And Electronic",
+      "name":"IEEE TRANSACTIONS ON KNOWLEDGE AND DATA ENGINEERING",
+      "topics":"",
+      "impact_factor":6.977,
+      "comments":"JCR"
+   },
+     {
+      "Q":"Q1 - MANAGEMENT<br>Q1 - INFORMATION SCIENCE & LIBRARY SCIENCE<br>Q1 - Electrical And Electronic",
+      "name":"Journal of Knowledge Management",
+      "topics":"",
+      "impact_factor":8.182,
+      "comments":"JCR"
+   },
+     {
+      "Q":"Q2 - ARTIFICIAL INTELLIGENCE<br>Q2 - INFORMATION SYSTEMS",
+      "name":"DATA MINING AND KNOWLEDGE DISCOVERY",
+      "topics":"",
+      "impact_factor":3.670,
+      "comments":"JCR"
+   },
+     {
+      "Q":"Q2 - SOFTWARE ENGINEERING<br>Q3 - INFORMATION SYSTEMS",
+      "name":"ACM Transactions on the Web",
+      "topics":"",
+      "impact_factor":2.043,
+      "comments":"JCR"
+   },
+                     
 ]
